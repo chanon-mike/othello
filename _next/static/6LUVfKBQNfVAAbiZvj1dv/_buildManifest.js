f2ff46e223012be9c1670ca435e60e259d7854c7
chanon-mike <chanon-mike@users.noreply.github.com>
2023-05-24 09:16:02 +0000

    deploy: 3dfd92a0d42fdc1619ef6c649613bc9f4878fde0

---
+++ _next/static/6LUVfKBQNfVAAbiZvj1dv/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/e7f4eb3c43c683dd.css","static/chunks/pages/index-0935a493dc235113.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/TbnPxqZquwkdEOyI4dcRa/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/c20e2e884d5e24ea.css","static/chunks/pages/index-00b397001bbb2dfc.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
